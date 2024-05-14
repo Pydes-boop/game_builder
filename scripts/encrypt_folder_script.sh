ab6a7d3772e86701f29c4f0a55a4cf81b76a6a16
Florence <59734957+Pydes-boop@users.noreply.github.com>
2024-05-14 20:26:29 +0200

    fix: file naming issue

--- scripts/encrypt_folder_script.sh
+++ scripts/encrypt_folder_script.sh
@@ -3,12 +3,12 @@
 # Set your input and output file paths <- we just set those directly
 # INPUT_FILE="build/${PLATFORM}"  # Assuming your built game is in the 'build' directory
 # OUTPUT_FILE="encrypted_build/${PLATFORM}.enc"  # Assuming you want to save the encrypted file in the 'encrypted_build' directory
-
+echo $FOLDER
 # Compress the folder into a zip file
-zip -r "$FOLDER.zip" "$FOLDER"
+zip -r $FOLDER.zip $FOLDER
 
 # Encrypt the zip file symmetrically using GPG
-gpg --quiet --batch --yes --symmetric --cipher-algo AES256 --passphrase="$PASSPHRASE" --output "$FOLDER.zip.enc" "$FOLDER.zip"
+gpg --quiet --batch --yes --symmetric --cipher-algo AES256 --passphrase=$PASSPHRASE --output $FOLDER.zip.enc $FOLDER.zip
 
 # Remove the temporary zip file
 rm "$FOLDER.zip"
